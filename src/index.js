cff90f607762b8574e60a6ba05e9c5c6e7bda3be
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-18 16:50:40 +0300

    Комбинация двух редьюсеров

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
-const reducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   if (action.type === "INCREMENT") {
     return state + 1;
   }
@@ -8,7 +8,20 @@ const reducer = (state = 0, action) => {
   return state;
 };
 
-const store = createStore(reducer);
+const todosReducer = (state = [], action) => {
+  if (action.type === "ADD_TODO") {
+    return [...state, "Learn Redux"];
+  }
+
+  return state;
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todos: todosReducer,
+});
+
+const store = createStore(rootReducer);
 
 console.log(store.getState());
 
